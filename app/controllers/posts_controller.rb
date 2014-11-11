966e023cae584cc328fd21c2eb15f3cdcdc7d012
Dave Strus <dave@davestrus.com>
2014-11-10 19:15:25 -0500

    Create posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,14 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(params.require(:post).permit(:title, :link, :body))
+    post.save
+    redirect_to posts_path
+  end
+
 end
